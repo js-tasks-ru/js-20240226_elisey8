1ccdc0f7dba555424e13a147e31a23816f09a053
Elis <Byhovtsov@gmail.com>
2024-03-31 22:25:12 +0300

    Task#7

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -3,5 +3,110 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart {
+  element = null;
+  data = []
+  subElements = {}
+  coefficent = null
 
+  constructor(options = {}) {
+    const {
+      url = "",
+      value = "",
+      link = "",
+      label = "",
+      formatHeading,
+      chartHeight = 50
+    } = options;
+    this.url = url;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.chartHeight = chartHeight;
+    this.formatHeading = formatHeading;
+    this.initialize();
+  }
+
+  async update(from = new Date(), to = new Date()) {
+    this.element.className = "column-chart_loading";
+    const params = new URLSearchParams({
+      from: from.toISOString(),
+      to: to.toISOString(),
+    });
+    const data = await fetch(`${BACKEND_URL}/${this.url}?` + params);
+    const res = await data.json();
+    this.element.className = "column-chart";
+    this.data = Object.values(res);
+    this.element.dispatchEvent(new CustomEvent("update-chart", { bubbles: true }));
+    return res;
+  }
+
+  initialize() {
+    document.addEventListener("update-chart", this.updateElement);
+    const chartElement = document.createElement("div");
+    chartElement.innerHTML = this.template.trim();
+    this.element = chartElement.firstChild;
+    this.initializeSubElements();
+    this.update();
+  }
+
+  render = (container) => {
+    container.append(this.element);
+  }
+
+  updateElement = () => {
+    this.subElements.body.innerHTML = this.createChartTemplate();
+  }
+
+  initializeSubElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+    elements.forEach(element => this.subElements[element.dataset.element] = element);
+  }
+
+  get template() {
+    return `
+        <div data-element="chart" class="column-chart" style="--chart-height: ${this.chartHeight}">
+          <div class="column-chart__title">
+            ${this.label}
+            <a href=${this.link} class="column-chart__link">View all</a>
+          </div>
+          <div data-element="container" class="column-chart__container">
+            <div data-element="header" class="column-chart__header">${this.value}</div>
+            <div data-element="body" class="column-chart__chart">
+              ${this.createChartTemplate()}
+            </div>
+          </div>
+        </div>
+      `;
+  }
+
+  createChartTemplate() {
+    return this.data
+      .map((value) =>
+        `<div
+              style="--value: ${this.getColumnValue(value)}"
+              data-tooltip=${this.getColumnPercent(value)}
+            ></div>`
+      ).join("");
+  }
+
+  remove() {
+    this.element?.remove();
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener("update-chart", this.updateElement);
+    this.element = null;
+  }
+
+  getColumnValue(value) {
+    if (!this.coefficent) {
+      this.coefficent = Math.max(...this.data) / this.chartHeight;
+    }
+    return Math.floor(value / this.coefficent);
+  }
+
+  getColumnPercent(value) {
+    return (value / Math.max(...this.data) * 100).toFixed(0) + '%';
+  }
 }
--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -1,20 +1,119 @@
-import fetchJson from './utils/fetch-json.js';
+import SortableTableV2 from '../../06-events-practice/1-sortable-table-v2/index.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
+const defaultSorted = (headerConfig) => ({
+  id: headerConfig.find(item => item.sortable).id,
+  order: 'asc'
+});
 
-export default class SortableTable {
+export default class SortableTable extends SortableTableV2 {
+  element = null
+  subElements = {}
+
   constructor(headersConfig, {
+    isSortLocally = false,
+    url = "",
     data = [],
     sorted = {}
   } = {}) {
-
+    super(headersConfig, { data, sorted: defaultSorted(headersConfig) });
+    this.headerConfig = headersConfig;
+    this.isSortLocally = isSortLocally;
+    this.sorted = sorted;
+    this.url = url;
+    this.data = data;
+    this.initialize();
   }
 
-  sortOnClient (id, order) {
-
+  initialize() {
+    const headerElement = super.createHeader(this.headerConfig);
+    const tableElement = super.createTableBody(this.data);
+    this.element = super.createRootElement(headerElement, tableElement);
+    this.initializeSubElements();
+    this.render();
+    this.addListeners();
   }
 
-  sortOnServer (id, order) {
+  async render() {
+    this.subElements.body.innerHTML = "loading...";
+    const data = await this.getTableData({
+      sort: this.sorted.id,
+      order: this.sorted.order,
+    });
+    this.data = data;
+    this.updateTable(data);
+  }
 
+  addListeners() {
+    this.subElements.header.addEventListener("pointerdown", this.handleSort);
+  }
+
+  updateTable(data) {
+    this.subElements.body.innerHTML = data.map(item => super.createTableRow(item)).join("");
+  }
+
+  async getTableData({
+                       sort,
+                       order,
+                       start = 0,
+                       end = 30
+                     }) {
+    const params = new URLSearchParams({
+      _sort: sort,
+      _order: order,
+      _start: start,
+      _end: end,
+    });
+
+    const data = await fetch(`${BACKEND_URL}/${this.url}?` + params);
+    return await data.json();
+  }
+
+  initializeSubElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+    elements.forEach(element => this.subElements[element.dataset.element] = element);
+  }
+
+  handleSort = (event) => {
+    const targetElement = event.target.closest("div");
+    const orderDirection = targetElement.dataset.order === "asc" ? "desc" : "asc";
+    const fieldName = targetElement.dataset.id;
+    targetElement.dataset.order = orderDirection;
+    const sort = this.isSortLocally ? this.sortOnClient : this.sortOnServer;
+    sort(fieldName, orderDirection);
+  }
+
+  sortOnClient = (fieldValue, orderValue) => {
+    const isSortable = this.headerConfig.find(item => item.id === fieldValue)?.sortable;
+    if (!isSortable) {return;}
+    const sortType = this.headerConfig.find(item => item.id === fieldValue).sortType;
+    const sortAscending = (a, b) => a[fieldValue] - b[fieldValue];
+    const sortDescending = (a, b) => b[fieldValue] - a[fieldValue];
+    const sortStringAscending = (a, b) => a[fieldValue].localeCompare(b[fieldValue], "ru");
+    const sortStringDescending = (a, b) => b[fieldValue].localeCompare(a[fieldValue], "ru");
+    if (sortType === "number") {
+      this.data.sort(orderValue === "asc" ? sortAscending : sortDescending);
+    } else {
+      this.data.sort(orderValue === "asc" ? sortStringAscending : sortStringDescending);
+    }
+    this.updateTable(this.data);
+  }
+
+  sortOnServer = async (id, order) => {
+    this.subElements.body.innerHTML = "loading...";
+    const data = await this.getTableData({
+      sort: id,
+      order,
+    });
+    this.data = data;
+    this.updateTable(data);
+  }
+
+  remove() {
+    this.element?.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
